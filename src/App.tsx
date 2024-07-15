d1ed26656031a31320d987c57d91de9b07cae216
Baguser7 <bagushendrawan01@gmail.com>
2024-07-15 22:58:53 +0700

    form-produk

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import { Outlet } from "@tanstack/react-router";
 import { Toaster } from "./components/toaster";
+import useStore from "./z-context";
+import { useEffect } from "react";
+import Axios from "axios"
 
 function App() {
+  // const user = useStore((state) => state.user)
+  const setUser = useStore((state) => state.SET_USER)
+  const token = localStorage.getItem("token");
+  useEffect(() => {
+    async function auth(){
+      try {
+        const response = await Axios({
+          method: "get",
+          url: `http://localhost:3000/login/auth`,
+          headers: { 
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+           },
+        });
+        return setUser(response.data);
+      } catch (error) {
+          return localStorage.removeItem("token");
+      }
+    }
+    auth();
+  },[])
   return (
     <>
-        <div className='w-full bg-gradient-to-br from-rose-600 to-red-600 h-screen overflow-y-auto'>
+        <div className='w-full bg-slate-800 h-screen overflow-y-auto'>
           {/* <img src="/bg.jpg" className="w-full h-full z-0 absolute object-cover bg-cover"></img> */}
           <Outlet /> 
         </div>
--- src/z-context.tsx
+++ src/z-context.tsx
@@ -1,14 +1,40 @@
 import { create } from 'zustand'
 
+type userType = {
+    id: string,
+    name: string,
+    email: string,
+    phone: string,
+    role_id: number,
+    isVerified: boolean,
+}
+
 type Store = {
-    user: string
-    SET_USER: (newUser : string) => void
+    user: userType
+    SET_USER: (newUser : userType) => void
     logout: () => void
   }
 const useStore = create<Store>() ((set) => ({
-    user: "",
-    SET_USER: (newUser : string) => set(({ user: newUser })),
-    logout: () => set({ user: "" }),
+    user: {
+      id: "",
+      name: "",
+      email:"",
+      phone: "",
+      role_id: NaN,
+      isVerified: false,
+    },
+    SET_USER: (newUser : userType) => set(({ user: newUser })),
+    logout: () => {
+      set({ user: {
+      id: "",
+      name: "",
+      email:"",
+      phone: "",
+      role_id: NaN,
+      isVerified: false,
+    },});
+    localStorage.removeItem("token");
+  },
 }))
 
 export default useStore;
